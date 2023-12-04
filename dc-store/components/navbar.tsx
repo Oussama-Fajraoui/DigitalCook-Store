29b9a5fa22b8b663f1a6522fcdbbedd06821f50f
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2023-12-04 14:48:44 +0100

    Menu items

---
+++ dc-store/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4"></div>
+    </div>
+  );
+};
+
+export default Navbar;
